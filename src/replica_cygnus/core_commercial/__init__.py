c52242cf6c6ffcb5d128408a4c52ac21046f0102
Diego Di Natale <49383324+dinatalediego@users.noreply.github.com>
2026-08-18 01:54:12 -0500

    feat: add core commercial package

---
+++ src/replica_cygnus/core_commercial/__init__.py
@@ -0,0 +1,7 @@
+from .service import core_status, ensure_core_commercial, refresh_core_commercial
+
+__all__ = [
+    "core_status",
+    "ensure_core_commercial",
+    "refresh_core_commercial",
+]
