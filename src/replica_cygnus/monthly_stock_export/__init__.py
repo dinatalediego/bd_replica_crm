a291546a6fb9cd952bac8aefa873019fee169810
Diego Di Natale <49383324+dinatalediego@users.noreply.github.com>
2026-09-09 16:20:58 -0500

    feat: add monthly stock export package

---
+++ src/replica_cygnus/monthly_stock_export/__init__.py
@@ -0,0 +1,11 @@
+from .service import (
+    export_monthly_stock_excel,
+    install_monthly_stock_sql,
+    validate_monthly_stock_dependencies,
+)
+
+__all__ = [
+    "export_monthly_stock_excel",
+    "install_monthly_stock_sql",
+    "validate_monthly_stock_dependencies",
+]
